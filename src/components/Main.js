70dbb46487a96ea44c5869ef3a003dff5c750a24
JohnnyLin518 <117791171+JohnnyLin518@users.noreply.github.com>
2022-11-29 16:31:05 -0800

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,56 @@
+import React, { useState } from 'react';
+import {Route, Switch, Redirect} from "react-router";
 
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import BuyerView from "./BuyerView";
+import ItemDetails from "./ItemDetails";
+import SellerView from "./SellerView";
+import PostItem from "./PostItem";
+import EditItem from "./EditItem"
+
+function Main(props){
+    const { isLoggedIn, handleLoggedIn } = props;
+
+    const showLogin = () => {
+        return isLoggedIn ? (
+          <Redirect to="/home" />
+        ) : (
+          <Login handleLoggedIn={handleLoggedIn} />
+        );
+      };     
+
+    const showItemDetails =() => {
+        return isLoggedIn ? <ItemDetails /> : <Redirect to = "/login" />;
+    };
+    
+    const showSellerView = () => {
+        return isLoggedIn ? <SellerView /> : <Redirect to = "/login" />;
+    };
+
+    const showPostItem =() => {
+        return isLoggedIn ? <PostItem /> : <Redirect to = "/login" />;
+    };
+
+    const showEditItem =() => {
+        return isLoggedIn ? <EditItem /> : <Redirect to = "/login" />;
+    };
+
+    return (
+        <div className = "main">
+            <Switch>
+                <Route path = "/home" component = { Home } />
+                <Route path = "/login" render = { showLogin } />
+                <Route path = "/register" component =  { Register } />
+                <Route path = "/BuyerView" component = { BuyerView } />
+                <Route path = "/ItemDetails" render = { showItemDetails } />
+                <Route path = "/SellerView" render = { showSellerView } />
+                <Route path = "/PostItem" render = { showPostItem } /> 
+                <Route path = "/EditItem" render = { showEditItem } />
+            </Switch>
+        </div>
+    );
+}
+
+export default Main;
