2789ef1a61053f6c80a5fb2076e32a037a6cf325
JohnnyLin518 <117791171+JohnnyLin518@users.noreply.github.com>
2022-12-06 12:41:40 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from "react";
-import Layout from "./components/Layouts/Layout";
+import { useState } from "react";
 
-import "./App.css";
-const App = () => {
-  return <Layout />;
-};
+import Buyerview from "./components/Buyerview";
+import Edititem from "./components/Edititem";
+import Home from "./components/Home";
+import Itemdetails from "./components/Itemdetails";
+import Login from "./components/Login";
+import Postitem from "./components/Postitem";
+import Sellerview from "./components/Sellerview";
+import Signup from "./components/Signup";
+import Logo from "./assets/images/logo.svg"
 
-export default App;
+import { LogoutOutlined } from '@ant-design/icons';
+import { Layout } from "antd";
+
+const { Header, Content} = Layout;
+
+function App() {
+
+  const [authed, setAuthed] = useState(false);
+
+  return (
+    <Layout style = {{ height: "100vh" }}>
+        <Header style = {{ backgroundColor: "lightblue" }}>
+          <div className = "header">
+            <img src = { Logo } className = "App-logo" alt = "logo" height = "64px"/>
+          </div>
+        </Header>
+        {/*<Content
+          style={{
+            padding: "50px",
+            maxHeight: "calc(100% - 64px)",
+            overflowY: "auto",
+          }}>
+            {authed ? ( <Edititem /> ) : ( <Login onSuccess = {() => setAuthed(true)} />
+            )}
+            {authed ? ( <Itemdetails />) :( <Login onSuccess = {() => setAuthed(true)} />
+            )}
+            {authed ? ( <Postitem />) :( <Login onSuccess = {() => setAuthed(true)} />
+            )}
+            {authed ? ( <Sellerview />) :( <Login onSuccess = {() => setAuthed(true)} />
+            )}
+        </Content>*/}
+    </Layout>
+  );
