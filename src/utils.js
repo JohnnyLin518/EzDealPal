0e4b243984fb09cf1297d5cb73e3dada9641d12d
JohnnyLin518 <117791171+JohnnyLin518@users.noreply.github.com>
2022-12-09 00:52:39 -0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -30,5 +30,70 @@ export const login = (credential) => {
     });
   };
 
-  
+
+  export const getProducts = (zipcode, distance) => {
+  return fetch(`/search?zip_code=${zipcode}&distance=${distance}`).then(
+    (response) => {
+      if (response.status < 200 || response.status >= 300) {
+        throw Error("Fail to getproducts");
+      }
+      return response.json();
+    }
+  );
+};
+
+export const getAllProducts = () => {
+  return fetch(`/search?zip_code=66952&distance=2000`).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to getproducts");
+    }
+    return response.json();
+  });
+};
+
+export const getSellerView = (UserID) => {
+  return fetch(`/sell/${UserID}`).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to get SellerView");
+    }
+    return response.json();
+  });
+};
+
+export const getItemDetails = (ProductID) => {
+  return fetch(`/itemdetails/${ProductID}`).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to get ItemDetail");
+    }
+    return response.json();
+  });
+};
+
+export const postItem = (data) => {
+  return fetch("/additem", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to post item");
+    }
+  });
+};
+
+// export const editItem = (ProductID) => {
+//   return fetch(`/EditItem/${ProductID}`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   }).then((response) => {
+//     if (response.status < 200 || response.status >= 300) {
+//       throw Error("Fail to edit item");
+//     }
+//   });
+// };
   
